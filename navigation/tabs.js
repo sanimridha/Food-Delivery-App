bdc6f760dfdb332284d532069b4a6347a15b4f5f
sanimridha <sanimridha307@gmail.com>
2021-02-03 15:52:34 +0600

    development structure

---
+++ navigation/tabs.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View, Image, TouchableOpacity} from 'react-native';
+import {
+  createBottomTabNavigator,
+  BottomTabBar,
+} from '@react-navigation/bottom-tabs';
+
+import {Home} from '../screens';
+import {COLORS, icons} from '../constants';
+
+const tabs = () => {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default tabs;
