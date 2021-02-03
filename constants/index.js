f6bbf619387e77dcbca031b307bf4a6155c6a0fa
sanimridha <sanimridha307@gmail.com>
2021-02-03 12:34:55 +0600

    calling constants into index.js

---
+++ constants/index.js
@@ -0,0 +1,7 @@
+import images from './images';
+import icons from './icons';
+
+import {COLORS, SIZES, FONTS} from './theme';
+import GOOGLE_API_KEY from './maps';
+
+export {images, icons, COLORS, FONTS, SIZES, GOOGLE_API_KEY};
---
+++ constants/maps.js
@@ -0,0 +1,3 @@
+const GOOGLE_API_KEY = 'AIzaSyD5Qw3kNiCyAkfZlOIdFOyGtijBNZvoy9g';
+
+export default GOOGLE_API_KEY;
