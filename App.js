ce0ee828f85e074a923f51b0560780a8ebb899ac
sanimridha <sanimridha307@gmail.com>
2021-02-03 15:58:41 +0600

    importing Tabs into app.js

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 
+import Tabs from './navigation/tabs';
 import {Home, Resturants, OrderDelivery} from './screens';
 
 const Stack = createStackNavigator();
@@ -12,7 +13,7 @@ const App = () => {
       <Stack.Navigator
         screenOptions={{headerShown: false}}
         initialRouteName={Home}>
-        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Home" component={Tabs} />
         <Stack.Screen name="Resturants" component={Resturants} />
         <Stack.Screen name="OrderDelivery" component={OrderDelivery} />
       </Stack.Navigator>
