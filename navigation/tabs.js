bc45c4ec95476182d4ae059107b9bf62bc00da08
sanimridha <sanimridha307@gmail.com>
2021-02-06 11:31:41 +0600

    Adding Search Like User tabs

--- navigation/tabs.js
+++ navigation/tabs.js
@@ -33,6 +33,57 @@ const tabs = () => {
           ),
         }}
       />
+      <Tab.Screen
+        name="Search"
+        component={Home}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <Image
+              source={icons.search}
+              resizeMode="contain"
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: focused ? COLORS.primary : COLORS.secondary,
+              }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Like"
+        component={Home}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <Image
+              source={icons.like}
+              resizeMode="contain"
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: focused ? COLORS.primary : COLORS.secondary,
+              }}
+            />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="User"
+        component={Home}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <Image
+              source={icons.user}
+              resizeMode="contain"
+              style={{
+                width: 25,
+                height: 25,
+                tintColor: focused ? COLORS.primary : COLORS.secondary,
+              }}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
