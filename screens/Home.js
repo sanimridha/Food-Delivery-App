37741757b732c158f37011e649e10dfe8d1f21bc
sanimridha <sanimridha307@gmail.com>
2021-02-06 18:04:53 +0600

    implementing Home screen top left button

--- screens/Home.js
+++ screens/Home.js
@@ -1,12 +1,48 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Image,
+  FlatList,
+  SafeAreaView,
+} from 'react-native';
+import {COLORS, icons, images, SIZES, FONTS} from '../constants';
+
+const renderHeader = () => (
+  <View style={{flexDirection: 'row', height: 50}}>
+    <TouchableOpacity
+      style={{
+        width: 50,
+        paddingLeft: SIZES.padding * 2,
+        justifyContent: 'center',
+      }}>
+      <Image
+        source={icons.nearby}
+        resizeMode="center"
+        style={{width: 30, height: 30}}
+      />
+    </TouchableOpacity>
+  </View>
+);
 
 const Home = () => {
-  return (
-    <View>
-      <Text></Text>
-    </View>
-  );
+  return <SafeAreaView style={styles.container}>{renderHeader()}</SafeAreaView>;
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.lightGray4,
+  },
+  shadow: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+  },
+});
 
 export default Home;
