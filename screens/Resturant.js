27f9a37d00f18cd02557f61b3dd221940692c525
sanimridha <sanimridha307@gmail.com>
2021-02-15 14:18:29 +0600

    added parent return into renderDots

--- screens/Resturant.js
+++ screens/Resturant.js
@@ -186,49 +186,52 @@ const Resturant = ({route, navigation}) => {
   };
   const renderDots = () => {
     const dotPosition = Animated.divide(scrollX, SIZES.width);
-    <View
-      style={{
-        height: 30,
-      }}>
+
+    return (
       <View
         style={{
-          flexDirection: 'row',
-          alignItems: 'center',
-          justifyContent: 'center',
-          height: SIZES.padding,
+          height: 30,
         }}>
-        {resturant?.menu.map((item, index) => {
-          const opacity = dotPosition.interpolate({
-            inputRange: [index - 1, index, index + 1],
-            outputRange: [0.3, 1, 0.3],
-            extrapolate: 'clamp',
-          });
-          const dotSize = dotPosition.interpolate({
-            inputRange: [index - 1, index, index + 1],
-            outputRange: [SIZES.base * 0.8, 10, SIZES.base * 0.8],
-            extrapolate: 'clamp',
-          });
-          const dotColor = dotPosition.interpolate({
-            inputRange: [index - 1, index, index + 1],
-            outputRange: [COLORS.darkgray, COLORS.primary, COLORS.darkgray],
-            extrapolate: 'clamp',
-          });
-          return (
-            <Animated.View
-              key={`dot-${index}`}
-              opacity={opacity}
-              style={{
-                borderRadius: SIZES.radius,
-                marginHorizontal: 6,
-                width: dotSize,
-                height: dotSize,
-                backgroundColor: dotColor,
-              }}
-            />
-          );
-        })}
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'center',
+            height: SIZES.padding,
+          }}>
+          {resturant?.menu.map((item, index) => {
+            const opacity = dotPosition.interpolate({
+              inputRange: [index - 1, index, index + 1],
+              outputRange: [0.3, 1, 0.3],
+              extrapolate: 'clamp',
+            });
+            const dotSize = dotPosition.interpolate({
+              inputRange: [index - 1, index, index + 1],
+              outputRange: [SIZES.base * 0.8, 10, SIZES.base * 0.8],
+              extrapolate: 'clamp',
+            });
+            const dotColor = dotPosition.interpolate({
+              inputRange: [index - 1, index, index + 1],
+              outputRange: [COLORS.darkgray, COLORS.primary, COLORS.darkgray],
+              extrapolate: 'clamp',
+            });
+            return (
+              <Animated.View
+                key={`dot-${index}`}
+                opacity={opacity}
+                style={{
+                  borderRadius: SIZES.radius,
+                  marginHorizontal: 6,
+                  width: dotSize,
+                  height: dotSize,
+                  backgroundColor: dotColor,
+                }}
+              />
+            );
+          })}
+        </View>
       </View>
-    </View>;
+    );
   };
   const renderOrder = () => {
     return <View>{renderDots()}</View>;
