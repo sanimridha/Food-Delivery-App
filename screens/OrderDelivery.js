4edfef2e7e752c4fb008addc857d0855473251b5
sanimridha <sanimridha307@gmail.com>
2021-02-16 12:52:40 +0600

    calling GOOGLE MAPS

--- screens/OrderDelivery.js
+++ screens/OrderDelivery.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import {View, Text} from 'react-native';
+import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 
 const OrderDelivery = () => {
   const renderMap = () => {
-    return <View></View>;
+    return (
+      <View style={{flex: 1}}>
+        <MapView style={{flex: 1}}></MapView>
+      </View>
+    );
   };
 
   return (
