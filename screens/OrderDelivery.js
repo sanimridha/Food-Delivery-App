b7c9f1a5450f1c26a749f8ced2e14740c47b264c
sanimridha <sanimridha307@gmail.com>
2021-02-16 13:25:00 +0600

    for map data

--- screens/OrderDelivery.js
+++ screens/OrderDelivery.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, Text, Image, TouchableOpacity} from 'react-native';
 import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 
@@ -10,6 +10,26 @@ const OrderDelivery = ({route, navigation}) => {
   const [fromLocation, setFromLocation] = useState(null);
   const [toLocation, setToLocation] = useState(null);
   const [region, setRegion] = useState(null);
+
+  useEffect(() => {
+    let {resturant, currentLocation} = route.params;
+
+    let fromLoc = currentLocation.gps;
+    let toLoc = resturant.location;
+    let street = currentLocation.streetName;
+
+    let mapRegion = {
+      latitude: (fromLoc.latitude + toLoc.latitude) / 2,
+      longitude: (fromLoc.longitude + toLoc.longitude) / 2,
+      latitudeDelta: Math.abs(fromLoc.latitude - toLoc.latitude) * 2,
+      longitudeDelta: Math.abs(fromLoc.longitude - toLoc.longitude) * 2,
+    };
+    setResturant(resturant);
+    setStreetName(street);
+    setFromLocation(fromLoc);
+    setToLocation(toLoc);
+    setRegion(mapRegion);
+  }, []);
 
   const renderMap = () => {
     return (
