104bbdeeb799a2e66d6e30c2afd3946cad6450ca
sanimridha <sanimridha307@gmail.com>
2021-02-16 13:04:52 +0600

    adding params

--- screens/OrderDelivery.js
+++ screens/OrderDelivery.js
@@ -4,7 +4,7 @@ import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 
 import {COLORS, FONTS, SIZES, GOOGLE_API_KEY} from '../constants';
 
-const OrderDelivery = () => {
+const OrderDelivery = ({route, navigation}) => {
   const renderMap = () => {
     return (
       <View style={{flex: 1}}>
