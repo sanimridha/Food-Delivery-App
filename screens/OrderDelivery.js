56f16cac60e35b540f7d513cb600c2660a107b41
sanimridha <sanimridha307@gmail.com>
2021-02-16 13:07:58 +0600

    state variable added

--- screens/OrderDelivery.js
+++ screens/OrderDelivery.js
@@ -5,6 +5,12 @@ import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
 import {COLORS, FONTS, SIZES, GOOGLE_API_KEY} from '../constants';
 
 const OrderDelivery = ({route, navigation}) => {
+  const [resturant, setResturant] = useState(null);
+  const [streetName, setStreetName] = useState('');
+  const [fromLocation, setFromLocation] = useState(null);
+  const [toLocation, setToLocation] = useState(null);
+  const [region, setRegion] = useState(null);
+
   const renderMap = () => {
     return (
       <View style={{flex: 1}}>
