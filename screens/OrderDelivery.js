22f584a6a02823e91ef29ef7cc7819b33ec55ac1
sanimridha <sanimridha307@gmail.com>
2021-02-21 23:32:54 +0600

    implemented renderDestinationHeader

--- screens/OrderDelivery.js
+++ screens/OrderDelivery.js
@@ -145,6 +145,46 @@ const OrderDelivery = ({route, navigation}) => {
       </View>
     );
   };
+  const renderDestinationHeader = () => {
+    return (
+      <View
+        style={{
+          position: 'absolute',
+          top: 50,
+          left: 0,
+          right: 0,
+          height: 50,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <View
+          style={{
+            flexDirection: 'row',
+            alignItems: 'center',
+            width: SIZES.width * 0.9,
+            paddingVertical: SIZES.padding,
+            paddingHorizontal: SIZES.padding * 2,
+            borderRadius: SIZES.radius,
+            backgroundColor: COLORS.white,
+          }}>
+          <Image
+            source={icons.red_pin}
+            style={{
+              width: 30,
+              height: 30,
+              marginRight: SIZES.padding,
+            }}
+          />
+
+          <View style={{flex: 1}}>
+            <Text style={{...FONTS.body3}}>{streetName}</Text>
+          </View>
+
+          <Text style={{...FONTS.body3}}>{Math.ceil(duration)} mins</Text>
+        </View>
+      </View>
+    );
+  };
 
   return (
     <View
@@ -152,6 +192,7 @@ const OrderDelivery = ({route, navigation}) => {
         flex: 1,
       }}>
       {renderMap()}
+      {renderDestinationHeader()}
     </View>
   );
 };
